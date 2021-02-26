87c8c93226f324805d1540d9fd09e988a32d8c53
Nicola Rankin <nicola.rankin@gmail.com>
2021-02-27 11:19:49 +1300

    import added to ready_tech

--- lib/ready_tech.rb
+++ lib/ready_tech.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 module ReadyTech
+  # Processes baskets: imports from csv, calculates tax, and prints reciepts
   class ReadyTech
+    def self.process_baskets
+      # import baskets
+      Dir.glob(files_to_import).each do |file_path|
+        items = CsvImporter.import(file_path)
+      end
+      # calculate taxes
+      # print reciepts
+      'success'
+    end
+
+    def self.files_to_import
+      File.join('data', '*.csv')
+    end
   end
 end
---
+++ spec/lib/ready_tech_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'ready_tech/ready_tech'
+
+RSpec.describe ReadyTech::ReadyTech do
+  it 'doesnt raise an error' do
+    expect { described_class.process_baskets }.not_to raise_error
+  end
+
+  it 'prints a success message' do
+    message = described_class.process_baskets
+    expect(message).to eq('success')
+  end
+end
