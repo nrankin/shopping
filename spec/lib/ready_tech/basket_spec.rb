371582f51a27ec4ce4a29f4f35e604ef5c6a1f49
Nicola Rankin <nicola.rankin@gmail.com>
2021-02-28 10:40:55 +1300

    test against output 3

--- spec/lib/ready_tech/basket_spec.rb
+++ spec/lib/ready_tech/basket_spec.rb
@@ -6,28 +6,34 @@ require 'ready_tech/basket'
 RSpec.describe ReadyTech::Basket do
   let(:items) do
     [
-      ReadyTech::Item.new({ quantity: 1, product: 'sweater', price: '22.34' }),
-      ReadyTech::Item.new({ quantity: 2, product: 'books', price: '32.50' }),
-      ReadyTech::Item.new({ quantity: 1, product: 'headache pills', price: '12.45' })
+      ReadyTech::Item.new({ quantity: 1, product: 'imported bottle of perfume', price: '27.99' }),
+      ReadyTech::Item.new({ quantity: 2, product: 'bottle of perfume', price: '18.99' }),
+      ReadyTech::Item.new({ quantity: 1, product: 'packet of headache pills', price: '9.75' }),
+      ReadyTech::Item.new({ quantity: 1, product: 'box of imported chocolates', price: '11.25' })
     ]
   end
 
-  let(:basket) { described_class.new(items) }
+  let!(:basket) { described_class.new(items) }
 
   describe '#receipt' do
     it 'creates a receipt' do
       basket.calculate_taxes
+      # puts "basket receipt: #{basket.receipt}"
       expect(basket.receipt).not_to be_nil
     end
 
     it 'has a correct total tax' do
+      basket.items.each {|item| p item.price.format }
       basket.calculate_taxes
-      expect(basket.receipt).to include('Sales Taxes: 3.48')
+      puts "basket receipt: #{basket.receipt}"
+
+      expect(basket.receipt).to include('Sales Taxes: 6.70')
     end
 
     it 'has a correct total' do
       basket.calculate_taxes
-      expect(basket.receipt).to include('Total: 70.77')
+      # puts "basket receipt: #{basket.receipt}"
+      expect(basket.receipt).to include('Total: 74.68')
     end
   end
 end
